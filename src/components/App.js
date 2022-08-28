a85d03438c02f0f6a84505c7aefd827316e79f4b
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-29 00:01:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   const [time, setTime] = useState("")
   const addTime = (e)=>{
-    const input = parseInt( "e.target.value")
+    const input = e.target.value
     setTime(input)
     if(input<0){
       setTime("0")
